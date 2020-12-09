2fa598448abba90e2c8c998def4731c48c1bb93c
zaffre <zaffre001@gmail.com>
2020-12-09 13:50:10 +0900

    attach ckeditor5

---
+++ app.js
@@ -0,0 +1,10 @@
+DecoupledEditor
+.create( document.querySelector( '#editor' ) )
+.then( editor => {
+	const toolbarContainer = document.querySelector( '#toolbar-container' );
+
+	toolbarContainer.appendChild( editor.ui.view.toolbar.element );
+} )
+.catch( error => {
+	console.error( error );
+} );
